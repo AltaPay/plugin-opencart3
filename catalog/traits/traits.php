22202f7cbd48c94e0473be501e75b7b1ce51aa16
Mahad Ashraf <>
2023-10-31 16:55:27 +0500

    update version to 3.12

--- catalog/traits/traits.php
+++ catalog/traits/traits.php
@@ -16,7 +16,7 @@ trait traitTransactionInfo
             'ecomPlatform'         => 'OpenCart',
             'ecomVersion'          => VERSION,
             'altapayPluginName'    => 'AltaPay',
-            'altapayPluginVersion' => '3.11',
+            'altapayPluginVersion' => '3.12',
             'otherInfo'            => $otherinfo,
         );
 
